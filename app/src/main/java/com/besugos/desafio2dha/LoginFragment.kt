7cf37379277c678f8720b71b9d65371c20e82326
Wendel Bandeira <besugos@gmail.com>
2020-11-09 12:43:47 -0300

    Register Layout

--- app/src/main/java/com/besugos/desafio2dha/LoginFragment.kt
+++ app/src/main/java/com/besugos/desafio2dha/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
 
 
 class LoginFragment : Fragment() {
@@ -16,6 +19,18 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        val btnRegister: Button = view.findViewById(R.id.btnRegisterLogin)
+
+        btnRegister.setOnClickListener {
+            navController.navigate(R.id.registerFragment)
+        }
     }
 
 }
--- app/src/main/java/com/besugos/desafio2dha/RegisterFragment.kt
+++ app/src/main/java/com/besugos/desafio2dha/RegisterFragment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.view.SupportActionModeWrapper
+import androidx.appcompat.widget.Toolbar
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.Navigation
+
+
 
 
 class RegisterFragment : Fragment() {
@@ -16,5 +24,23 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        val toolbar: Toolbar = view.findViewById(R.id.tbRegister)
+
+        toolbar.setNavigationOnClickListener {
+            navController.navigate(R.id.loginFragment)
+        }
+
+        view.findViewById<Button>(R.id.btnRegisterRegister).setOnClickListener {
+
+            navController.navigate(R.id.loginFragment)
+        }
+    }
+
 
 }
